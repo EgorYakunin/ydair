0edf1cec0bf785d786f568a96ce5ccc05d7985ec
EgorYakunin <e.yakunin04@gmail.com>
2022-12-12 12:39:41 +0300

    init commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,37 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import Container from '@/components/std/Container';
+import Spacer from '@/components/std/Spacer';
+
+import plane_illustation from '@/assets/img/7x.svg';
+import half_plane_illustation from '@/assets/img/half.svg';
+import styles from './test.module.css';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 13!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://beta.nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js 13</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  )
+    return (
+        <>
+            <Spacer top="5" bottom="5"/>
+            <Image
+                src={half_plane_illustation}
+                alt="Plane illustation"
+                className={styles.img}
+            />
+            <Container>
+                <p>New York</p>
+                <h1>NYC</h1>
+                <p>San Francisco</p>
+                <h1>SFO</h1>
+                <Image
+                    src={plane_illustation}
+                    alt="Plane illustation"
+                    className={styles.img}
+                />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Amet accusantium facere dicta voluptates est voluptatum
+                    asperiores, corrupti recusandae aliquid. Eaque.
+                </p>
+            </Container>
+            <Spacer bottom="25"/>
+        </>
+    );
 }
